0b623f6184898c03e10d2225b065794084b05e73
YummyPotatoPie <53563234+YummyPotatoPie@users.noreply.github.com>
2020-06-09 09:11:46 +0300

    Ключевые слова

---
+++ KEY_WORDS.py
@@ -0,0 +1,24 @@
+KEY_WORDS = [
+    'prod',
+    'sum',
+    'out',
+    'in',
+    'div',
+    'pow',
+    'func'
+]
+
+REGISTERS = [
+    'RX', 'TX',
+    'DX', 'FX',
+    'CX', 'VX',
+    'FRX', 'FTX', 
+    'FDX', 'FFX', 
+    'FCX', 'FVX' 
+]
+ERRORS = [
+    'ERROR: Invalid Syntax.',
+    'ERROR: Invalid Register.',
+    'ERROR: Division By Zero.',
+    'ERROR: Invalid Types.'
+]
