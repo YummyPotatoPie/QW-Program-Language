efedf3faaf75d0833331bb99214b339be64d98e9
YummyPotatoPie <53563234+YummyPotatoPie@users.noreply.github.com>
2020-06-09 09:12:37 +0300

    Виртуальные регистры

---
+++ REGISTERS.py
@@ -0,0 +1,37 @@
+RX = 0
+TX = 0
+DX = 0
+FX = 0
+CX = 0
+VX = 0
+
+DATA_REGISTERS = [
+    RX, TX,
+    DX, FX,
+    CX, VX
+]
+
+DATA_REGISTERS_LINK = [
+    'RX', 'TX',
+    'DX', 'FX',
+    'CX', 'VX'
+]
+
+FRX = []
+FTX = []
+FDX = []
+FFX = []
+FCX = []
+FVX = []
+
+FUNC_REGISTERS = [
+    FRX, FTX, 
+    FDX, FFX, 
+    FCX, FVX 
+]
+
+FUNC_REGISTERS_LINK = [
+    'FRX', 'FTX', 
+    'FDX', 'FFX', 
+    'FCX', 'FVX' 
+]
